aa33722cd4dbb52d70b18798193153a2af3b151c
Anthony Garvan <anthony.garvan@gmail.com>
2015-02-06 11:49:07 -0500

    using fast tsne, 20k words, visualization tweaks

--- bundle.js
+++ bundle.js
@@ -11302,7 +11302,7 @@ module.exports = function (graph, layout) {
   var width = window.innerWidth,
       height = window.innerHeight;
 
-  var stage = new PIXI.Stage(0xFFFFFF, true);
+  var stage = new PIXI.Stage(0x000000, true);
   var renderer = PIXI.autoDetectRenderer(width, height, null, false, true);
   renderer.view.style.display = "block";
   document.body.appendChild(renderer.view);
@@ -11311,8 +11311,8 @@ module.exports = function (graph, layout) {
   graphics.position.x = 0;
   graphics.position.y = 0;
 
-  graphics.scale.x = .5;
-  graphics.scale.y = .5;
+  graphics.scale.x = 1;
+  graphics.scale.y = 1;
   stage.addChild(graphics);
 
   return {
@@ -11326,29 +11326,30 @@ module.exports = function (graph, layout) {
     stage: stage
   };
 }
-var testData;
+var wordMap;
 function drawGraph(graphics) {
   // No magic at all: Iterate over positions array and render nodes/links
-
+  var width = window.innerWidth;
   var i, x, y, x1, y1;
   
-  if(testData) {
-      graphics.lineStyle(0);
-      graphics.clear();
-      graphics.beginFill(0xFF3300);
-        var xScale = window.innerWidth;
-    for(var wordEntry in testData) {
-        x = xScale*testData[wordEntry].x - 5;
-        y = xScale*testData[wordEntry].y - 5;
-        graphics.drawRect(x, y, 10, 10);
+  if(wordMap) {
+    graphics.lineStyle(0);
+    graphics.clear();
+      
+    var scale = window.innerHeight;
+    for(var wordEntry in wordMap) {
+        x = scale*(wordMap[wordEntry].x-0.5) + width/2;
+        y = scale*wordMap[wordEntry].y;
+        graphics.beginFill(0xFFFFFF);
+        graphics.drawRect(x, y, 1, 1);
+        graphics.endFill();
     };
-    graphics.drawRect(0, 0, 100, 100);
   } else {
         var $ = require('jquery');
         $.ajax({
              url:    '/testData.json',
              success: function(result) {
-                          testData = result;
+                          wordMap = result;
                       },
              async:   false
         });
--- main.py
+++ main.py
@@ -1,15 +1,55 @@
 import json
 import random
+#from sklearn.manifold import TSNE
+from tsne import bh_sne
+from numpy.random import rand
+from sklearn.preprocessing import MinMaxScaler
+from time import time
+import numpy as np
 
-wordmap = {}
-for i in range(1000):
-  key = "word-number-%d" % i
-  x = (random.random() + random.random() + random.random())/3
-  y = (random.random() + random.random() + random.random())/3
+start_time = time()
+n_samples = 20000
+#wordlist = ["word-%d" % i for i in range(n_samples)]
+#print "creating random samples..."
+#X_raw = rand(n_samples,250)
+
+f = open("data/vectors0.txt", 'r')
+
+isHeader = True
+X_list = []
+word_list = []
+for line in f:
+  if not isHeader:
+    entries = line.strip().split(' ')
+    #print "length: %d" % len(entries)
+    word_list.append(entries[0])
+    stringVector = entries[1:]
+    #print stringVector
+    v = [float(entry) for entry in stringVector]
+    X_list.append(v)
+  isHeader = False
   
+X_raw = np.array(X_list[:n_samples])
+
+print X_raw
+print "transforming data to two dimensions..."
+X_2d = bh_sne(X_raw)
+X_scaled = MinMaxScaler().fit_transform(X_2d)
+#X_scaled = X_2d
+wordmap = {}
+
+print "formatting data..."
+for i in range(0, len(X_scaled)):
+  x = X_scaled[i][0]
+  y = X_scaled[i][1]
+  key = word_list[i]
   wordmap[key] = {"x": x, "y": y}
   
 
 f = open('testData.json', 'w')
 json.dump(wordmap, f)
 f.close()
+
+end_time = time()
+duration = float(end_time - start_time) / 60
+print "Duration: %f minutes." % duration
